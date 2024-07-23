e5baf38aa88836289de022388c5af224958433c4
Dipak Zende <114248721+dipak307@users.noreply.github.com>
2024-07-23 21:44:01 +0530

    Update globalContext.js

--- fronteds/src/context/globalContext.js
+++ fronteds/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "https://mernexpense.vercel.app/api/v1/";
 
 const GlobalContext = React.createContext();
 
